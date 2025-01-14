61e53b68c00a4a708c024604b72c78d195bd9aa1
Sem <semik.kolesnikovv@gmail.com>
2025-01-14 18:33:28 +0100

    Initial Commit

---
+++ Projectr5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projectr5
+//
+//  Created by Sem Koliesnikov on 14/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
